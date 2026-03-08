1b99b7b316606bbba217ca30cef3f2d2f2074ade
miss-b4teman <ocf2@kent.ac.uk>
2026-03-08 11:38:00 +0000

    updating main

--- main.py
+++ main.py
@@ -9,3 +9,27 @@ for p in passwords:
 
 analyser = passwordAnalyser(p)
 print(analyser.length())
+
+from files import readPasswords, writeResults
+from passwordanalyser import PasswordAnalyser
+
+password = readPassword("Passwords.txt")
+
+results = []
+
+for p in passwords:
+
+    analyser = PasswordAnalyser(p)
+
+    results.append([
+        p,
+        analyser.score()
+        analyser.rating()
+        analyser.guessTime()
+        analyser.suggestions()
+    ])
+
+writeResults("results.csv", results)
+print("All Passwords Analysed")
+
+#Here the main function brings everything together
