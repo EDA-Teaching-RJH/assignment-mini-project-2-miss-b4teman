3f76e73da630b954abe70ac95879da4a50397ff4
miss-b4teman <ocf2@kent.ac.uk>
2026-03-08 12:49:21 +0000

    debugging: syntax

--- passwordanalyser.py
+++ passwordanalyser.py
@@ -7,80 +7,80 @@ class PasswordAnalyser:
     def __init__(self, password):
         self.password = password
 
-def length(self):
-    return len(self.password)
+    def length(self):
+        return len(self.password)
 
 #Regular expression: importing the library (do that at the top) and checking the passwords for different features
 
-def uppercase(self):
-    return bool(re.search(r"[A-Z]", self.password))
+    def uppercase(self):
+        return bool(re.search(r"[A-Z]", self.password))
 
-def lowercase(self):
-    return bool(re.search(r"[a-z]", self.password))
+    def lowercase(self):
+        return bool(re.search(r"[a-z]", self.password))
 
-def numbers(self):
-    return bool(re.search(r"\d", self.password))
+    def numbers(self):
+        return bool(re.search(r"\d", self.password))
 
-def specialCharacters(self):
-    return bool(re.search(r"[!-/:-@[-`{-~]", self.password))
-    #How to regex ALL special characters.. 
-    #https://stackoverflow.com/questions/18057962/regex-pattern-including-all-special-characters
-    #look intocompiling regex
+    def specialCharacters(self):
+        return bool(re.search(r"[!-/:-@[-`{-~]", self.password))
+        #How to regex ALL special characters.. 
+        #https://stackoverflow.com/questions/18057962/regex-pattern-including-all-special-characters
+        #look intocompiling regex
 
 #scoring passwords + giving rating
-def score(self):
-    score = 0
+    def score(self):
+        score = 0
  
-    if self.length() >= 8:
-        score += 1
+        if self.length() >= 8:
+            score += 1
     
-    if self.uppercase():
-        score += 1
+        if self.uppercase():
+            score += 1
 
-    if self.lowercase():
-        score += 1
+        if self.lowercase():
+            score += 1
 
-    if self.numbers():
-        score += 1
+        if self.numbers():
+            score += 1
 
-    if self.specialCharacters():
-        score += 1
+        if self.specialCharacters():
+            score += 1
 
-    return score
+        return score
 
-def rating(self):
-    rate = self.score()
+    def rating(self):
+        rate = self.score()
 
-    if rate <= 2:
-        return "Weak Password"
+        if rate <= 2:
+            return "Weak Password"
     
-    elif rate == 3:
-        return "Medium Password"
+        elif rate == 3:
+            return "Medium Password"
 
-    elif rate <= 4:
-        return "Strong Password"
+        elif rate <= 4:
+            return "Strong Password"
 
-    elif rate == 5:
-        return "Very Strong Password"
+        elif rate == 5:
+            return "Very Strong Password"
 
-    else:
-        return "ERROR"
+        else:
+            return "ERROR"
 
 #Suggestions for password, tips is empty, but if it doesnt fit a criteria it will add the related suggestion
-def suggestions(self):
+    def suggestions(self):
     
-    tips = []
+        tips = []
     
-    if not self.uppercase():
-        tips.append("Add uppercase letters")
+        if not self.uppercase():
+            tips.append("Add uppercase letters")
 
-    if not self.numbers():
-        tips.append("Add numbers")
+        if not self.numbers():
+            tips.append("Add numbers")
 
-    if not self.specialCharacters():
-        tips.append("Add special characters")
+        if not self.specialCharacters():
+            tips.append("Add special characters")
 
-    if self.length() < 12:
-        tips.append("Add more characters/Make password longer")
+        if self.length() < 12:
+            tips.append("Add more characters/Make password longer")
 
-    return ", ".join(tips)
+        return ", ".join(tips)
