3d53be29908b4512a6c717335d60bb4748ce2fca
miss-b4teman <ocf2@kent.ac.uk>
2026-03-08 11:29:17 +0000

    writing results to cvs

--- files.py
+++ files.py
@@ -7,4 +7,21 @@ def readPasswords(filePath):
 
     return passwords
 
+
+#Writing the results to my results csv file using all results from the passwordanalyster, the O in file i/o
 def writeResults(filePath, results):
+
+    with open(filePath, "w", newline="") as file:
+
+        writer = csv.writer(file)
+
+        writer.writerow([
+            "Password"
+            "Score"
+            "Rating"
+            "Guess time"
+            "Suggestions"
+        ])
+
+        for r in results:
+            writer.writerow(r)
