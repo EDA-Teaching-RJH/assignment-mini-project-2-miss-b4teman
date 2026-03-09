767c2b8565d2c36b2a2c8dde4b71f33a760a62fd
miss-b4teman <ocf2@kent.ac.uk>
2026-03-09 13:37:40 +0000

    testing

--- test_PasswordAnalyser.py
+++ test_PasswordAnalyser.py
@@ -3,59 +3,59 @@ import pytest
 from passwordanalyser import PasswordAnalyser
 
 #testing diff features:
-def testLength():
+def test_Length():
     p = PasswordAnalyser("HelloTest!!!!")
     assert p.length() == 12
 
-def testUppercase():
+def test_Uppercase():
     p = PasswordAnalyser("Testingg")
     assert p.uppercase() == True
 
-def testLowercase():
+def test_Lowercase():
     p = PasswordAnalyser("tEsTiNg")
     assert p.lowercase() == True
 
-def testNumbers():
+def test_Numbers():
     p = PasswordAnalyser("test67")
     assert p.numbers() == True
 
-def testSpecial():
+def test_Special():
     p = PasswordAnalyser("Test:P")
     assert p.specialCharacters() == True
 
 #regex edge cases:
-def testNoNumber():
+def test_NoNumber():
     p = PasswordAnalyser("Test")
     assert p.numbers() == False
 
-def testNoSpecialChars():
+def test_NoSpecialChars():
     p = PasswordAnalyser("TestPassword1245")
     assert p.specialCharacters() == False
 
-def testRepeated():
+def test_Repeated():
     p = PasswordAnalyser("hehehehe1212")
     assert p.repeatedCharacters() == True
 
 #score tets:
-def testWeak():
+def test_Weak():
     p = PasswordAnalyser("password")
     assert p.score() == 0
 
-def testStrong():
+def test_Strong():
     p = PasswordAnalyser("FunH4ppyT3st!!!:D")
     assert p.score() == 5
 
 #entropy test:
-def testGoodEntropy():
+def test_GoodEntropy():
     p = PasswordAnalyser("Test?@12LOLc4t")
     assert p.entropy() > 0
 
-def testEmpty():
+def test_Empty():
     p = PasswordAnalyser("")
     assert p.entropy() == 0
 
 #Testing the code:
-def testCode():
+def test_Code():
     try:
         p = PasswordAnalyser("")
         p.score()
